71fddab96a5a8e80b836f0e0ca2ff0cc2b79017c
emoltz <es5541@nyu.edu>
2022-10-17 12:43:11 -0400

    database

--- swe2_app/settings.py
+++ swe2_app/settings.py
@@ -77,11 +77,15 @@ WSGI_APPLICATION = 'swe2_app.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'testdb_01',
+            'USER': 'postgres',
+            'PASSWORD': 'missinglink',
+            'HOST': 'localhost',
+            'PORT': '5432',
+        }
     }
-}
 
 
 # Password validation
