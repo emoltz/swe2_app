1e54e8d91a1b771a964ecc25e4f66f861e15d08e
emoltz <es5541@nyu.edu>
2022-10-18 09:55:08 -0400

    first django test

--- polls/tests.py
+++ polls/tests.py
@@ -14,3 +14,12 @@ class QuestionModelsTests(TestCase):
         time = timezone.now() + datetime.timedelta(days=30)
         future_question = Question(pub_date=time)
         self.assertIs(future_question.was_published_recently(), False)
+
+
+class UnitTestCase(TestCase):
+    def test_one(self):
+        self.assertEqual(1, 1)
+
+    def test_home_homepage_template(self):
+        response = self.client.get('/') # this is the homepage
+        self.assertTemplateUsed(response, 'polls/base.html')
