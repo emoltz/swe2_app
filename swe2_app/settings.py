499407c16ad0d1ab7985584d1e93a7a704744fe0
emoltz <es5541@nyu.edu>
2022-10-18 09:18:54 -0400

    coveralls

--- swe2_app/settings.py
+++ swe2_app/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = "django-insecure-8lj_7%u(7u7cu&=*(1$%h^*h_jlh1g9b$0x7m+l+bd#r$k_ho_"
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
